5f30ab53eeca38e5b0bf00dcc54691ce1d3f43d2
jackberck <berckjack@gmail.com>
2024-12-12 12:48:34 +0700

    init: project setup

---
+++ app/Models/Image.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $fillable = [
+        'path',
+        'post_id',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
